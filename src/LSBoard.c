59c8c068a94ec3ff1ca0b4f9367f6719ba105e43
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-21 16:39:52 +0100

    Fixed ull in bitwise shift

--- src/LSBoard.c
+++ src/LSBoard.c
@@ -1,8 +1,6 @@
 
 #include "LSBoard.h"
 
-#include <string.h>
-
 ls_board_t ls_board_init() {
     const auto board = (ls_board_t)calloc(1, sizeof(struct _ls_board_t));
 
@@ -18,7 +16,7 @@ void ls_board_print(const ls_board_t board) {
 
     for (uint64_t row = 0; row < 8; row++) {
         for (uint64_t col = 0; col < 8; col++) {
-            if (string[row * 8 + col] == '\0') {
+            if (string[row * 8 + col] == 0) {
                 printf(".");
             } else {
                 printf("%c", string[row * 8 + col]);
@@ -31,12 +29,12 @@ void ls_board_print(const ls_board_t board) {
 }
 
 void ls_board_start(const ls_board_t board) {
-    board->black_king = 0x8;
-    board->black_queen = 0x10;
-    board->black_rock = 0x81;
-    board->black_knight = 0x42;
-    board->black_bishop = 0x24;
-    board->black_pawn = 0xff00;
+    board->white_king = 0x8;
+    board->white_queen = 0x10;
+    board->white_rock = 0x81;
+    board->white_knight = 0x42;
+    board->white_bishop = 0x24;
+    board->white_pawn = 0xff00;
 
     board->black_king = 0x800000000000000;
     board->black_queen = 0x1000000000000000;
@@ -47,47 +45,34 @@ void ls_board_start(const ls_board_t board) {
 }
 
 char* ls_board_to_string(const ls_board_t board) {
-    const auto string = (char*)calloc(1, sizeof(char));
+    const auto string = (char*)calloc(64, sizeof(char));
 
     for (uint8_t i = 0; i < 64; i++) {
-        const uint64_t and = (1 << i);
-
+        const uint64_t and = 1ULL << 63 - i;
 
         if (board->white_king & and) {
             string[i] = 'K';
-        }
-        if (board->white_queen & and) {
+        } else if (board->white_queen & and) {
             string[i] = 'Q';
-        }
-        if (board->white_rock & and) {
+        } else if (board->white_rock & and) {
             string[i] = 'R';
-        }
-        if (board->white_knight & and) {
+        } else if (board->white_knight & and) {
             string[i] = 'N';
-        }
-        if (board->white_bishop & and) {
+        } else if (board->white_bishop & and) {
             string[i] = 'B';
-        }
-        if (board->white_pawn & and) {
+        } else if (board->white_pawn & and) {
             string[i] = 'P';
-        }
-
-        if (board->black_king & and) {
+        } else if (board->black_king & and) {
             string[i] = 'k';
-        }
-        if (board->black_queen & and) {
+        } else if (board->black_queen & and) {
             string[i] = 'q';
-        }
-        if (board->black_rock & and) {
+        } else if (board->black_rock & and) {
             string[i] = 'r';
-        }
-        if (board->black_knight & and) {
+        } else if (board->black_knight & and) {
             string[i] = 'n';
-        }
-        if (board->black_bishop & and) {
+        } else if (board->black_bishop & and) {
             string[i] = 'b';
-        }
-        if (board->black_pawn & and) {
+        } else if (board->black_pawn & and) {
             string[i] = 'p';
         }
     }
