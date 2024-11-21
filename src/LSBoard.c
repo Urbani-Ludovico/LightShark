a7c8f71fd291407eec68177b076b71b4e6db7e82
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-21 14:58:07 +0100

    Board start configuration

--- src/LSBoard.c
+++ src/LSBoard.c
@@ -11,4 +11,19 @@ void ls_board_destroy(const ls_board_t board) {
     free(board);
 }
 
-void ls_board_start(ls_board_t board) {}
+void ls_board_start(const ls_board_t board) {
+    board->white_king = 0x8;
+    board->white_queen = 0x10;
+    board->white_rock = 0x81;
+    board->white_knight = 0x42;
+    board->white_bishop = 0x24;
+    board->white_pawn = 0xff00;
+
+    board->black_king = 0x800000000000000;
+    board->black_queen = 0x1000000000000000;
+    board->black_rock = 0x8100000000000000;
+    board->black_knight = 0x4200000000000000;
+    board->black_bishop = 0x2400000000000000;
+    board->black_pawn = 0xff000000000000;
+    
+}
