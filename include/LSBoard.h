9d00d6b151cc720da026ba5dfacd9e6555610dcf
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-21 14:43:05 +0100

    Board struct

--- include/LSBoard.h
+++ include/LSBoard.h
@@ -3,8 +3,25 @@
 #define BOARD_H
 
 #include <stdlib.h>
+#include <stdint.h>
 
-struct _ls_board_t {};
+typedef uint64_t ls_board_state_t;
+
+struct _ls_board_t {
+    ls_board_state_t white_king;
+    ls_board_state_t white_queen;
+    ls_board_state_t white_rock;
+    ls_board_state_t white_knight;
+    ls_board_state_t white_bishop;
+    ls_board_state_t white_pawn;
+
+    ls_board_state_t black_king;
+    ls_board_state_t black_queen;
+    ls_board_state_t black_rock;
+    ls_board_state_t black_knight;
+    ls_board_state_t black_bishop;
+    ls_board_state_t black_pawn;
+};
 
 typedef struct _ls_board_t* ls_board_t;
 
