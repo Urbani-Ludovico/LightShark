ad0195c88eba2966735684cd415359adedd02e3b
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-21 14:23:37 +0100

    Renamed variable

--- src/LSBoard.c
+++ src/LSBoard.c
@@ -6,9 +6,9 @@ struct _ls_board_t {
 };
 
 ls_board_t ls_board_init() {
-    const auto engine = (ls_board_t)calloc(1, sizeof(ls_board_t));
+    const auto board = (ls_board_t)calloc(1, sizeof(ls_board_t));
 
-    return engine;
+    return board;
 }
 
 void ls_board_destroy(const ls_board_t board) {
