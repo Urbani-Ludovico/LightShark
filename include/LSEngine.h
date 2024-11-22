fb866ea44690ca08d922f1b3a2bc73aa90a0be3d
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-22 13:52:56 +0100

    Added game information

--- include/LSEngine.h
+++ include/LSEngine.h
@@ -6,9 +6,30 @@
 #include "LSBoard.h"
 #include "LSTree.h"
 
+enum _ls_player_t {
+    WHITE, BLACK
+};
+
+typedef enum _ls_player_t ls_player_t;
+
 struct _ls_t {
     ls_board_t board;
+    ls_state_t state;
     ls_state_t tree;
+
+    ls_player_t turn;
+    uint16_t moves_count;
+
+    uint8_t captured_white_queen;
+    uint8_t captured_white_rock;
+    uint8_t captured_white_knight;
+    uint8_t captured_white_bishop;
+    uint8_t captured_white_pawn;
+    uint8_t captured_black_queen;
+    uint8_t captured_black_rock;
+    uint8_t captured_black_knight;
+    uint8_t captured_black_bishop;
+    uint8_t captured_black_pawn;
 };
 
 typedef struct _ls_t* ls_t;
--- src/LSEngine.c
+++ src/LSEngine.c
@@ -4,11 +4,28 @@
 ls_t ls_init() {
     auto const engine = (ls_t)malloc(sizeof(struct _ls_t));
 
+    // Start tree
     engine->tree = ls_tree_init();
+    engine->state = engine->tree;
+
+    // Start first board
     engine->tree->board = ls_board_init();
     engine->board = engine->tree->board;
-
     ls_board_start(engine->board);
+
+    // Game status
+    engine->turn = WHITE;
+    engine->moves_count = 0;
+    engine->captured_white_queen = 0;
+    engine->captured_white_rock = 0;
+    engine->captured_white_knight = 0;
+    engine->captured_white_bishop = 0;
+    engine->captured_white_pawn = 0;
+    engine->captured_black_queen = 0;
+    engine->captured_black_rock = 0;
+    engine->captured_black_knight = 0;
+    engine->captured_black_bishop = 0;
+    engine->captured_black_pawn = 0;
 
     return engine;
 }
