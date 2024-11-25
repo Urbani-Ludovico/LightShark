a6af6ea6d2d6c2e3b5fbb65fe442cdad6ad6f261
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-25 17:56:38 +0100

    Bishop check

--- src/LSState.c
+++ src/LSState.c
@@ -18,8 +18,8 @@ ls_check_t ls_state_is_check(ls_state_t const state) {
 bool ls_state_is_board_check(ls_board_t const board, ls_player_t const player) {
     ls_board_state_t const king = player == WHITE ? board->black_king : board->white_king;
     ls_board_state_t const queen = player == WHITE ? board->white_queen : board->black_queen;
-    // ls_board_state_t const bishop = player == WHITE ? board->white_bishop : board->black_bishop;
-    // ls_board_state_t const knight = player == WHITE ? board->white_knight : board->black_knight;
+    ls_board_state_t const bishop = player == WHITE ? board->white_bishop : board->black_bishop;
+    ls_board_state_t const knight = player == WHITE ? board->white_knight : board->black_knight;
     // ls_board_state_t const rook = player == WHITE ? board->white_rock : board->black_rock;
     // ls_board_state_t const pawn = player == WHITE ? board->white_pawn: board->black_pawn;
 
@@ -83,7 +83,35 @@ bool ls_state_is_board_check(ls_board_t const board, ls_player_t const player) {
         current_state &= occupied_mask;
     }
 
-    return false;
+    // Bishop
+    current_state = bishop;
+    for (uint8_t i = 0; i < 7 && current_state; i++) {
+        current_state &= 0x00FEFEFEFEFEFEFE;
+        current_state <<= 7;
+        if (current_state & king) return true;
+        current_state &= occupied_mask;
+    }
+    current_state = bishop;
+    for (uint8_t i = 0; i < 7 && current_state; i++) {
+        current_state &= 0xFEFEFEFEFEFEFE00;
+        current_state >>= 9;
+        if (current_state & king) return true;
+        current_state &= occupied_mask;
+    }
+    current_state = bishop;
+    for (uint8_t i = 0; i < 7 && current_state; i++) {
+        current_state &= 0x7F7F7F7F7F7F7F00;
+        current_state >>= 7;
+        if (current_state & king) return true;
+        current_state &= occupied_mask;
+    }
+    current_state = bishop;
+    for (uint8_t i = 0; i < 7 && current_state; i++) {
+        current_state &= 0x007F7F7F7F7F7F7F;
+        current_state <<= 9;
+        if (current_state & king) return true;
+        current_state &= occupied_mask;
+    }
 }
 
 ls_board_state_t ls_board_occupied_mask(ls_board_t const board) {
