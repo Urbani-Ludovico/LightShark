cd579f436ac86694675779dc44f55d2943803943
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-27 10:54:11 +0100

    Capture

--- src/LSGenereteMoves.c
+++ src/LSGenereteMoves.c
@@ -6,13 +6,26 @@
 #include "LSState.h"
 #include "LSTree.h"
 
+
 #ifndef LS_STATE_MOVES_GENERATE_INSERT_MOVE
 #define LS_STATE_MOVES_GENERATE_INSERT_MOVE(piece) \
     ls_board_t const new_board = ls_board_copy(state->board); \
     if (state->turn == LS_PLAYER_WHITE) { \
         new_board->white_##piece = new_##piece; \
+        new_board->black_king &= ~new_##piece; \
+        new_board->black_queen &= ~new_##piece; \
+        new_board->black_bishop &= ~new_##piece; \
+        new_board->black_knight &= ~new_##piece; \
+        new_board->black_rook &= ~new_##piece; \
+        new_board->black_pawn &= ~new_##piece; \
     } else { \
         new_board->black_##piece = new_##piece; \
+        new_board->white_king &= ~new_##piece; \
+        new_board->white_queen &= ~new_##piece; \
+        new_board->white_bishop &= ~new_##piece; \
+        new_board->white_knight &= ~new_##piece; \
+        new_board->white_rook &= ~new_##piece; \
+        new_board->white_pawn &= ~new_##piece; \
     } \
     if (!ls_state_is_board_check(new_board, state->turn)) { \
         ls_tree_insert_board_move(state, new_board); \
@@ -35,6 +48,7 @@ ls_state_moves_generation_status ls_state_moves_generate(ls_state_t const state)
     ls_board_state_t const bishop = state->turn == LS_PLAYER_WHITE ? state->board->white_bishop : state->board->black_bishop;
     ls_board_state_t const knight = state->turn == LS_PLAYER_WHITE ? state->board->white_knight : state->board->black_knight;
     ls_board_state_t const rook = state->turn == LS_PLAYER_WHITE ? state->board->white_rook : state->board->black_rook;
+    ls_board_state_t const pawn = state->turn == LS_PLAYER_WHITE ? state->board->white_pawn : state->board->black_pawn;
     for (uint8_t i = 0; i < 64; i++) {
         // King
         if (king & (1ULL << i)) {
