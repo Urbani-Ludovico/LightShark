9e4b8169365f8c449119a0dde4524c24da81f13d
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-27 08:54:00 +0100

    Fixed binary not

--- src/LSGenereteMoves.c
+++ src/LSGenereteMoves.c
@@ -13,7 +13,7 @@ ls_state_moves_generation_status ls_state_moves_generate(ls_state_t const state)
     if (state->moves_length != 0) return LS_STATE_GENERATION_MOVES_ALREADY_DONE;
     if (state->turn == LS_UNDEFINED_PLAYER) return LS_STATE_GENERATION_MOVES_PLAYER_UNDEFINED;
 
-    ls_board_state_t const occupied = state->turn == ~(LS_PLAYER_WHITE ? ls_board_occupied_mask_white(state->board) : ls_board_occupied_mask_black(state->board));
+    ls_board_state_t const occupied = ~(state->turn == LS_PLAYER_WHITE ? ls_board_occupied_mask_white(state->board) : ls_board_occupied_mask_black(state->board));
 
     // King
     ls_board_state_t const king = state->turn == LS_PLAYER_WHITE ? state->board->white_king : state->board->black_king;
@@ -50,7 +50,6 @@ ls_state_moves_generation_status ls_state_moves_generate(ls_state_t const state)
 
         king_shift >>= 1;
     }
-
 
     return state->moves_length > 0 ? LS_STATE_GENERATION_MOVES_DONE : LS_STATE_GENERATION_MOVES_EMPTY;
 }
