9cbd210dd585ceba0598e802e526f685e774a7cf
Ludovico Urbani <lu.urbani@outlook.com>
2024-11-27 09:50:13 +0100

    Generate queen moves

--- src/LSGenereteMoves.c
+++ src/LSGenereteMoves.c
@@ -12,14 +12,15 @@ ls_state_moves_generation_status ls_state_moves_generate(ls_state_t const state)
     if (state->turn == LS_UNDEFINED_PLAYER) return LS_STATE_GENERATION_MOVES_PLAYER_UNDEFINED;
 
     ls_board_state_t const empty_squares = ~(state->turn == LS_PLAYER_WHITE ? ls_board_occupied_mask_white(state->board) : ls_board_occupied_mask_black(state->board));
+    ls_board_state_t const opponent_positions = state->turn == LS_PLAYER_WHITE ? ls_board_occupied_mask_black(state->board) : ls_board_occupied_mask_white(state->board);
 
     // King
     ls_board_state_t const king = state->turn == LS_PLAYER_WHITE ? state->board->white_king : state->board->black_king;
     for (uint8_t i = 0; i < 64; i++) {
         if (king & (1ULL << i)) {
-            for (uint8_t j = 0; j < _ls_king_moves; j++) {
-                if (_ls_king_moves_from_masks[j] & (1ULL << i)) {
-                    ls_board_state_t new_king = (_ls_king_moves_directions[j] > 0 ? (1ULL << (i + _ls_king_moves_directions[j])) : (1ULL >> (i - _ls_king_moves_directions[j]))) & empty_squares;
+            for (uint8_t move = 0; move < _ls_king_moves; move++) {
+                if (_ls_king_moves_from_masks[move] & (1ULL << i)) {
+                    ls_board_state_t new_king = (_ls_king_moves_directions[move] > 0 ? (1ULL << (i + _ls_king_moves_directions[move])) : (1ULL >> (i - _ls_king_moves_directions[move]))) & empty_squares;
 
                     if (new_king) {
                         new_king |= (king & ~(1ULL << i));
@@ -36,6 +37,42 @@ ls_state_moves_generation_status ls_state_moves_generate(ls_state_t const state)
                         } else {
                             ls_board_destroy(new_board);
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    // Queen
+    ls_board_state_t const queen = state->turn == LS_PLAYER_WHITE ? state->board->white_queen : state->board->black_queen;
+    for (uint8_t i = 0; i < 64; i++) {
+        if (queen & (1ULL << i)) {
+            for (uint8_t move = 0; move < _ls_queen_moves; move++) {
+                ls_board_state_t new_queen = (1ULL << i);
+                for (uint8_t move_step = 0; move_step < _ls_queen_moves; move_step++) {
+                    if (_ls_queen_moves_from_masks[move] & new_queen) {
+                        new_queen = (_ls_king_moves_directions[move] > 0 ? (new_queen << _ls_king_moves_directions[move]) : (new_queen >> -_ls_king_moves_directions[move])) & empty_squares;
+
+                        if (new_queen) {
+                            new_queen |= (queen & ~(1ULL << i));
+
+                            ls_board_t const new_board = ls_board_copy(state->board);
+                            if (state->turn == LS_PLAYER_WHITE) {
+                                new_board->white_queen = new_queen;
+                            } else {
+                                new_board->black_queen = new_queen;
+                            }
+
+                            if (!ls_state_is_board_check(new_board, state->turn)) {
+                                ls_tree_insert_board_move(state, new_board);
+                            } else {
+                                ls_board_destroy(new_board);
+                            }
+
+                            if (new_queen & opponent_positions) {
+                                break;
+                            }
+                        } else break;
                     }
                 }
             }
